7a9179cee450d820193816bbca9499a595fc75e6
AhmadDalleh <ahmeddalleh8@gmail.com>
2025-05-04 11:23:16 +0300

    add class partial view

--- DotNetCoreMVCProject/Controllers/ClassController.cs
+++ DotNetCoreMVCProject/Controllers/ClassController.cs
@@ -1,6 +1,8 @@
 ﻿using DotNetCoreMVCProject.Data;
 using DotNetCoreMVCProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.Pkcs;
 
 namespace DotNetCoreMVCProject.Controllers
 {
@@ -20,5 +22,59 @@ namespace DotNetCoreMVCProject.Controllers
             List<TheClass> classes = _context.Classes.ToList();
             return View(classes);
         }
+
+        public IActionResult AddClass()
+        {
+            TheClass _class = new TheClass();
+            _class.Students.Add(new TheClassStudent());
+            return View("AddClass", _class);
+        }
+
+        [HttpPost]
+        public IActionResult AddClass(TheClass theClass)
+        {
+            _context.Classes.Add(theClass);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            var theClass = _context.Classes.Where(x => x.Id == id).Include(s => s.Students).FirstOrDefault();
+            return View(theClass);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var theClass = _context.Classes.Where(x => x.Id == id).Include(s => s.Students).FirstOrDefault();
+            return View(theClass);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TheClass theClass)
+        {
+            List<TheClassStudent> students = _context.Students.Where(x=>x.TheClassId == theClass.Id).ToList();
+            _context.Students.RemoveRange(students);
+            _context.SaveChanges();
+
+            _context.Classes.Update(theClass);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id) 
+        {
+            var theClass = _context.Classes.Where(x => x.Id == id).Include(s => s.Students).FirstOrDefault();
+            return View(theClass);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(TheClass theClass) 
+        {
+            _context.Attach(theClass);
+            _context.Entry(theClass).State = EntityState.Deleted;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
---
+++ DotNetCoreMVCProject/Controllers/ClassPopupController.cs
@@ -0,0 +1,38 @@
+﻿using DotNetCoreMVCProject.Data;
+using DotNetCoreMVCProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreMVCProject.Controllers
+{
+    public class ClassPopupController : Controller
+    {
+        public ClassPopupController(AppDbContext context, IConfiguration conf)
+        {
+            _context = context;
+            _conf = conf;
+        }
+
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _conf;
+
+        public IActionResult Index()
+        {
+            List<TheClass> classes = _context.Classes.ToList();
+            return View(classes);
+        }
+        public IActionResult AddClass()
+        {
+            TheClass theClass = new TheClass();
+            theClass.Students.Add(new TheClassStudent());
+            return PartialView("_AddClassPV",theClass);
+        }
+
+        [HttpPost]
+        public IActionResult AddClass(TheClass theClass)
+        {
+            _context.Classes.Add(theClass);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
