9b49162eb47263a1a77aa8be56e61c9cdd46ee96
ahmaddalleh <ahmaddalleh35@gmail.com>
2025-05-06 01:20:31 +0300

    add Email Sender

---
+++ DotNetCoreMVCProject/Controllers/EmailController.cs
@@ -0,0 +1,37 @@
+﻿using DotNetCoreMVCProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+
+namespace DotNetCoreMVCProject.Controllers
+{
+    public class EmailController : Controller
+    {
+        public IActionResult Index()
+        {
+            MdlEmail mdl = new MdlEmail();
+            return View(mdl);
+        }
+
+        [HttpPost]
+        public IActionResult Index(MdlEmail mdl)
+        {
+            MailMessage mailMessage = new MailMessage
+            {
+                From = new MailAddress(mdl.senderEmail!),
+                Subject = mdl.subject,
+                Body = mdl.body,
+                IsBodyHtml = false
+            };
+
+            SmtpClient smtpClient = new SmtpClient(mdl.smtpServer, mdl.smtpPort)
+            {
+                Credentials = new NetworkCredential(mdl.senderEmail, mdl.senderPassword),
+                EnableSsl = true
+            };
+
+            smtpClient.Send(mailMessage);
+            return View("index");
+        }
+    }
+}
---
+++ DotNetCoreMVCProject/Models/MdlEmail.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetCoreMVCProject.Models
+{
+    public class MdlEmail
+    {
+        public string? smtpServer { get; set; } = "smtp.gmail.com";
+
+        public int smtpPort { get; set; } = 587;
+
+        public string? senderEmail { get; set; }
+
+        public string? senderPassword { get; set; }
+
+        public string? recipientEmail { get; set; }
+
+        public string? subject { get; set; }
+
+        public string? body { get; set; }
+
+    }
+}
